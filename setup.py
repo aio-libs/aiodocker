9f97250f13c11e215cbfc916f016527fdbf1587c
Paul Tagliamonte <tag@pault.ag>
2014-04-05 12:29:03 -0400

    Initial import

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+long_description = open('README.md', 'r').read()
+
+setup(
+    name="aiodocker",
+    version="0.1",
+    packages=['aiodocker',],  # This is empty without the line below
+    author="Paul Tagliamonte",
+    author_email="paultag@debian.org",
+    long_description=long_description,
+    description='does some stuff with things & stuff',
+    license="Expat",
+    url="",
+    platforms=['any']
+)
