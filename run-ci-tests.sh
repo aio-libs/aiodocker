b926e643ec10b1fd9ba89fca21cbbbc4f22cf4bb
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-07-15 14:08:27 +0200

    Split requirements into several files

--- run-ci-tests.sh
+++ run-ci-tests.sh
@@ -2,7 +2,7 @@
 
 find * -name '*.py[co]' | xargs rm -f
 pip install pytest pytest-asyncio codecov pytest-cov
-pip install -r requirements.txt
+pip install -r requirements/dev.txt
 pip install -e .
 
 python -B -m pytest --cov=aiodocker --cov-report=xml tests
