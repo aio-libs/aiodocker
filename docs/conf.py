c4d1bbee0fc537fdda9140a5d1afb405d416a0b8
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-07-15 16:39:00 +0200

    Revert sidebars to default

--- docs/conf.py
+++ docs/conf.py
@@ -139,11 +139,13 @@ html_static_path = ['_static']
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'about.html', 'navigation.html', 'searchbox.html',
-    ]
-}
+
+
+# html_sidebars = {
+#     '**': [
+#         'about.html', 'navigation.html', 'searchbox.html',
+#     ]
+# }
 
 # html_sidebars = {
 #    '**': [
