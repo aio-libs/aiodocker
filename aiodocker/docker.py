0be85e6a49c6a7d6a0a2b19d1470d3ba9e333d53
Christian Barra <barrachri@gmail.com>
2017-07-19 09:58:17 +0200

    add support for python 3.5.x (#68)

--- aiodocker/docker.py
+++ aiodocker/docker.py
@@ -87,7 +87,7 @@ class Docker:
         self.volumes = DockerVolumes(self)
 
     async def close(self):
-        self.session.close()  # no longer coroutine since aiohttp 2.x
+        await self.session.close()
 
     async def auth(self, **credentials):
         response = await self._query_json(
@@ -500,7 +500,7 @@ class DockerEvents:
                 self._transform_event,
                 human_bool(params['stream']),
             )
-            async for data in self.json_stream.fetch():
+            async for data in self.json_stream:
                 await self.channel.publish(data)
         finally:
             # signal termination to subscribers
