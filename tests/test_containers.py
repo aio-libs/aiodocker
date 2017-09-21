8d9092183f54282c65648176a62822ce01a6a6a0
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-09-21 13:31:37 -0700

    add another testcase

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -1,9 +1,24 @@
 import pytest
 
 
+async def _validate_hello(container):
+    try:
+        await container.start()
+        response = await container.wait()
+        assert response['StatusCode'] == 0
+        logs = await container.log(stdout=True)
+        assert logs == ['hello']
+
+        with pytest.raises(TypeError):
+            await container.log()
+    finally:
+        await container.delete(force=True)
+
+
 @pytest.mark.asyncio
-async def test_run_container(docker):
+async def test_run_existing_container(docker):
     name = "alpine:latest"
+    await docker.pull(name)
     container = await docker.containers.run(
         config={
             'Cmd': ['-c', 'echo hello'],
@@ -12,11 +27,20 @@ async def test_run_container(docker):
         }
     )
 
-    try:
-        await container.start()
-        response = await container.wait()
-        assert response['StatusCode'] == 0
-        logs = await container.log(stdout=True, stderr=True)
-        assert logs == ['hello']
-    finally:
-        await container.delete(force=True)
+    await _validate_hello(container)
+
+
+@pytest.mark.asyncio
+async def test_run_missing_container(docker):
+    name = "alpine:latest"
+    await docker.images.delete(name)
+
+    container = await docker.containers.run(
+        config={
+            'Cmd': ['-c', 'echo hello'],
+            'Entrypoint': 'sh',
+            'Image': name
+        }
+    )
+
+    await _validate_hello(container)
