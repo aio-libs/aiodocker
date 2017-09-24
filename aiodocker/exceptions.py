dee976cb596b7a9b1c35b9a0e594f7f17f96f70b
Christian Barra <barrachri@gmail.com>
2017-09-24 15:30:52 +0200

    fix error #128

--- aiodocker/exceptions.py
+++ aiodocker/exceptions.py
@@ -1,9 +1,10 @@
 class DockerError(Exception):
 
-    def __init__(self, status, data, *args):
+    def __init__(self, status, data, container_id=None, *args):
         super().__init__(*args)
         self.status = status
         self.message = data['message']
+        self.container_id = container_id
 
     def __repr__(self):
         return 'DockerError({self.status}, {self.message!r})'.format(self=self)
