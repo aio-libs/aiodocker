8a2d4239b989e83f5ee777e2d415f2d20f43c4ff
joshua1b <joshua1b2823@gmail.com>
2017-10-28 15:14:19 +0900

    Implement #137.

--- aiodocker/exceptions.py
+++ aiodocker/exceptions.py
@@ -1,13 +1,24 @@
 class DockerError(Exception):
 
-    def __init__(self, status, data, container_id=None, *args):
+    def __init__(self, status, data, *args):
         super().__init__(*args)
         self.status = status
         self.message = data['message']
-        self.container_id = container_id
 
     def __repr__(self):
         return 'DockerError({self.status}, {self.message!r})'.format(self=self)
 
     def __str__(self):
         return 'DockerError({self.status}, {self.message!r})'.format(self=self)
+
+class DockerContainerError(DockerError):
+    
+    def __init__(self, status, data, container_id, *args):
+        super().__init__(status, data, *args)
+        self.container_id = container_id
+
+    def __repr__(self):
+        return 'DockerContainerError({self.status}, {self.message!r}, {self.container_id!r})'.format(self=self)
+
+    def __str__(self):
+        return 'DockerContainerError({self.status}, {self.message!r}, {self.container_id!r})'.format(self=self)
