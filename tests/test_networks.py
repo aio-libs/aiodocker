85ece8165023c66944c07a80a1a59c77d2fe3e28
Anton Patrushev <apatrushev@gmail.com>
2018-03-13 15:28:36 +0500

    network creation test improved

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -14,4 +14,6 @@ async def test_list_networks(docker):
 async def test_create_and_destroy_network(docker):
     network = await docker.networks.create({'Name': 'test-net'})
     assert isinstance(network, aiodocker.networks.DockerNetwork)
+    data = await network.show()
+    assert data['Name'] == 'test-net'
     await network.delete()
