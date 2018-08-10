2d84a61c749f86ac35067fe2f9c88d1b9ffb6703
Andrew Gaul <andrew@gaul.org>
2018-08-11 02:47:53 +0530

    Handle responses with 0 or missing Content-Length (#241)

--- aiodocker/utils.py
+++ aiodocker/utils.py
@@ -23,6 +23,9 @@ async def parse_result(response, response_type=None, *,
     if response_type is None:
         ct = response.headers.get('content-type')
         if ct is None:
+            cl = response.headers.get('content-length')
+            if cl is None or cl == '0':
+                return ''
             raise TypeError('Cannot auto-detect response type '
                             'due to missing Content-Type header.')
         main_type, sub_type, extras = parse_content_type(ct)
