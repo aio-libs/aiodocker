c76533c5a607441c5764d764a338600a311efacc
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-09-18 02:26:49 +0300

    Increase tests stability

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -12,7 +12,7 @@ async def _validate_hello(container):
         assert response["StatusCode"] == 0
         await asyncio.sleep(5)  # wait for output in case of slow test container
         logs = await container.log(stdout=True)
-        assert logs == ["hello\n"]
+        assert "hello\n" in logs
 
         with pytest.raises(TypeError):
             await container.log()
