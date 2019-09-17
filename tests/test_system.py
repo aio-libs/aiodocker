9453a099eed619fa9bfc11b4a214cd211b25c37c
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-09-17 08:56:54 +0300

    Skip failed test is DOCKER_VERSION is not set

--- tests/test_system.py
+++ tests/test_system.py
@@ -3,8 +3,14 @@ import os
 
 
 @pytest.mark.asyncio
-async def test_system_info(docker):
+async def test_system_info_id(docker):
     docker_info = await docker.system.info()
     assert "ID" in docker_info
+
+@pytest.mark.asyncio
+async def test_system_info_version(docker):
+    docker_info = await docker.system.info()
     docker_version = os.getenv("DOCKER_VERSION")
+    if docker_version is None:
+        pytest.skip("DOCKER_VERSION env var is not set")
     assert docker_version in docker_info["ServerVersion"]
