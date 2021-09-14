61d036197e5d3ecf05f119a087cc90f22f507461
Andrei Neagu <neagu@itis.swiss>
2021-09-14 12:51:25 +0200

    renamed network to avoid collisions

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -13,7 +13,9 @@ async def test_list_networks(docker):
 
 @pytest.mark.asyncio
 async def test_list_networks_with_filter(docker):
-    await docker.networks.create({"Name": "test-net", "Labels": {"some": "label"}})
+    await docker.networks.create(
+        {"Name": "test-net-filter", "Labels": {"some": "label"}}
+    )
     networks = await docker.networks.list(filters={"label": "some=label"})
     assert len(networks) == 1
 
