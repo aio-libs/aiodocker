1f9150ba98652f7728f1e7c2fd4c112e406933bd
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-07-17 10:30:44 -0600

    Use `TYPE_CHECKING` flag to avoid importing from `typing_extensions` at run time

--- aiodocker/types.py
+++ aiodocker/types.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import sys
 from typing import (
+    TYPE_CHECKING,
     Mapping,
     Protocol,
     Sequence,
@@ -9,7 +11,12 @@ from typing import (
     Union,
 )
 
-from typing_extensions import TypeAlias
+
+if TYPE_CHECKING:
+    if sys.version_info < (3, 10):
+        from typing_extensions import TypeAlias
+    else:
+        from typing import TypeAlias
 
 
 _T_co = TypeVar("_T_co", covariant=True)
