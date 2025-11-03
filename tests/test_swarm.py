ce864706533dda3018c82e60cea0bd0ecb94aeb1
Joongi Kim <me@daybreaker.info>
2025-11-03 22:18:35 +0900

    Isolate docker instances for swarm and service tests (#981)

--- tests/test_swarm.py
+++ tests/test_swarm.py
@@ -23,21 +23,29 @@ async def test_swarm_failing_joining(swarm):
 
 
 @pytest.mark.asyncio
-async def test_swarm_init(docker):
+async def test_swarm_init(dind_docker_host, testing_images):
     if sys.platform == "win32":
         pytest.skip("swarm commands dont work on Windows")
-    default_addr_pool = ["10.0.0.0/8"]
-    data_path_port = 4789
-    subnet_size = 24
-    assert await docker.swarm.init(
-        advertise_addr="127.0.0.1:2377",
-        data_path_port=data_path_port,
-        default_addr_pool=default_addr_pool,
-        subnet_size=subnet_size,
-        force_new_cluster=True,
-    )
-    swarm_inspect = await docker.swarm.inspect()
-    assert swarm_inspect["DefaultAddrPool"] == default_addr_pool
-    assert swarm_inspect["DataPathPort"] == data_path_port
-    assert swarm_inspect["SubnetSize"] == subnet_size
-    await docker.swarm.leave(force=True)
+
+    # Create a separate DinD client for this test
+    from aiodocker.docker import Docker
+
+    docker = Docker(url=dind_docker_host)
+    try:
+        default_addr_pool = ["10.0.0.0/8"]
+        data_path_port = 4789
+        subnet_size = 24
+        assert await docker.swarm.init(
+            advertise_addr="127.0.0.1:2377",
+            data_path_port=data_path_port,
+            default_addr_pool=default_addr_pool,
+            subnet_size=subnet_size,
+            force_new_cluster=True,
+        )
+        swarm_inspect = await docker.swarm.inspect()
+        assert swarm_inspect["DefaultAddrPool"] == default_addr_pool
+        assert swarm_inspect["DataPathPort"] == data_path_port
+        assert swarm_inspect["SubnetSize"] == subnet_size
+        await docker.swarm.leave(force=True)
+    finally:
+        await docker.close()
